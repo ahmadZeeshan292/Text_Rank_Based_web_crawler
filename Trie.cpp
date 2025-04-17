ceba2147eae3d6e65622719db3708d64101fbe63
ahmadZeeshan292 <itzahmad292@gmail.com>
2025-04-17 21:25:28 +0500

    Create Trie.cpp

---
+++ Trie.cpp
@@ -0,0 +1,84 @@
+#include "Trie.h"
+
+Trie::Trie()
+{
+	Root = new TrieNode();
+}
+
+void Trie::insert(std::string r, std::string file)
+{
+	TrieNode* current = Root;
+
+    for (int i = 0;i < r.size();i++) {
+		if (int(r[i]) < 0) continue;
+		bool isdig = isdigit(r[i]);
+
+		int s;
+		if (!isdig) {
+			s = isupper(r[i]) ? 'Z' - r[i] + 26 : r[i] - 'a';
+		}
+		else {
+			s = '0' - r[i];
+		}
+		
+		if (r[i] == ' ' || ispunct(r[i]) ||r[i] < 65) {
+			if (current->inverse_index.find(file) == current->inverse_index.end()) {
+				current->inverse_index[file] = 1;
+			}
+			else {
+				current->inverse_index[file] += 1;
+			} 
+			current = Root;
+			continue;
+		}
+
+		if (current->next[s] == nullptr) {
+			current->next[s] = new TrieNode(r[i]);
+			current = current->next[s];
+        }
+        else {
+            current = current->next[s];
+        }
+	}
+	if (current->inverse_index.find(file) == current->inverse_index.end()) {
+		current->inverse_index[file] = 1;
+	}
+	else {
+		current->inverse_index[file] += 1;
+	}
+}
+
+std::list<std::hash_map<std::string, int>> Trie::search(std::string r)
+{
+	std::list<std::hash_map<std::string, int>> freq_map;
+	TrieNode* current = Root;
+	for (int i = 0;i < r.size();i++) { // k
+
+		if (r[i] == ' ') { // k
+			freq_map.push_back(current->inverse_index);
+			current = Root;
+			continue;
+		}
+
+		bool isdig = isdigit(r[i]); // k
+		int s;
+
+		if (!isdig) { // k
+			s = isupper(r[i]) ? 'Z' - r[i] + 26 : r[i] - 'a';
+		}
+		else {
+			s = '0' - r[i] + 52;
+		}
+		if (current->next[s] == nullptr) { // k
+			return freq_map;
+		}
+		else {
+			current = current->next[s]; // k
+		}
+	}
+	freq_map.push_back(current->inverse_index); // 1
+	return freq_map; // k
+}
+
+
+
